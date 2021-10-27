959d60915ce6f53a7f3cc82b7f518749b93784bc
Gehrig Barnes <gehrigbarnes@gmail.com>
2021-10-27 15:37:23 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const main = document.querySelector('main')
+
+main.remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.setAttribute("id", "victory")
+
+newHeader.innerHTML = "Gehrig is the champion"
+
+document.body.append(newHeader)
